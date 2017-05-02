e16d44cadfff971fb2d4cd9e29cab6cd7af6f30c
Caelin Jackson-King <caelink@gmail.com>
2017-05-02 15:23:09 -0400

    First attempt at resource pls work no disgustingerino

---
+++ app.py
@@ -0,0 +1,18 @@
+#!flask/bin/python
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+opinions = {"marth":"disgusting"}
+
+@app.route('/')
+def index():
+    return "Welcome to the memes!"
+
+@app.route('/api/opinion')
+def opinion():
+    return jsonify({"opinions": opinions})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
